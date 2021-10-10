9d3caca7885c430b7a17f8bc70d4cb220e60cd27
MihaiDavid05 <44817698+MihaiDavid05@users.noreply.github.com>
2021-10-10 15:08:00 +0200

    Added an argument parser

--- helpers/config.py
+++ helpers/config.py
@@ -2,6 +2,11 @@ import yaml
 
 
 def read_config(config_file_path='configs/first_experiment.yaml'):
+    """
+    Read config file.
+    :param config_file_path:
+    :return:
+    """
     with open(config_file_path, 'r') as file:
         config = yaml.load(file, Loader=yaml.FullLoader)
     return config
--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from helpers.config import read_config
+import argparse
+
+CONFIGS_PATH = 'configs/'
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config_filename', type=str, help='Config file that you want to use during the run.')
+
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
-    c = read_config('configs/first_experiment.yaml')
+    args = parse_arguments()
+    config_path = CONFIGS_PATH + args.config_filename + '.yaml'
+    c = read_config(config_path)
 
-    print("OK")
+    print(c)
